46d2c1f03205867bd80237ed04d6894e071fb039
Beca <becanavarro2003@gmail.com>
2023-11-05 10:00:22 -0400

    Initial commit

---
+++ app/src/main/java/com/example/algoritmicaii/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.algoritmicaii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
